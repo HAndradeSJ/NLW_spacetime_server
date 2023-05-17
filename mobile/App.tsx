d47d3a0f15293a9b6329f4e92d768387346b8398
HAndradeSJ <henriqueandradesj@outlook.com>
2023-05-17 12:21:13 -0300

    version mobile config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import React from 'react'
 
 export default function App(): any {
+  // função que retorna um boleano enquanto as fonts nn carregar
+  const [hadsloadfonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+  if (!hadsloadfonts) {
+    return null
+  }
   return (
     <View className="flex-1 items-center justify-center bg-gray-900 ">
-      <Text className="text-5xl font-bold text-gray-50">Rocketseat</Text>
-      <StatusBar style="light"translucent />
+      <Text className="text-5xl font-bold text-gray-50 font-title">
+        Rocketseat
+      </Text>
+      <StatusBar style="light" translucent />
     </View>
   )
 }
-
