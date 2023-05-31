699184a6c33ef4aa090312f5c1f49c797401c8d3
HAndradeSJ <henriqueandradesj@outlook.com>
2023-05-30 22:53:21 -0300

    version servers

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,12 +1,17 @@
 // Importação
 import { FastifyInstance } from "fastify"
-import { Prisma } from "../lib/prisma";
-import {z} from 'zod';
-import axios from 'axios'
+import {randomUUID} from 'node:crypto'
+import { createWriteStream } from "node:fs"
+import {extname, resolve} from 'node:path'  
+import { pipeline } from "node:stream"
+import { promisify } from "node:util"
+
+
+const pump = promisify(pipeline)
 
 // rota de autenticação
 export default function UploadRoutes (app:FastifyInstance){
-    app.post('/upload', async (Request)=>{
+    app.post('/upload', async (Request,reply)=>{
             // pegando o arquivi da requisição
             const upload = await Request.file({
                 limits:{
@@ -17,8 +22,27 @@ export default function UploadRoutes (app:FastifyInstance){
             if(!upload){
                 return reply.status(400).send()
             }
-            upload
+            const mimetype = /^(imagem|video)\/[a-zA-Z]+/
+            const isvalidad = mimetype.test(upload.mimetype)
+            if(!isvalidad){
+                return reply.status(400).send()
+            }
+
+        // gerando um id da imagem 
+        const fileid = randomUUID()
+        // renomeando a imagem para não subistituir
+        const extesion = extname(upload.filename)
+        const Filename = fileid.concat(extesion)
+
+        // criando um streamm 
+        const writeStream = createWriteStream(
+            resolve(__dirname,'../../uploads/',Filename)
+        )
+        await pump(upload.file, writeStream)
+
+        return {ok:true}
     })
    
     
 }
+
